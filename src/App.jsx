5a6e2a9a800ab6489681eaa5e56becce79846c38
Shariful Islam <sajukhan12905@gmail.com>
2024-07-26 20:39:50 +0600

    uploading in git hub

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./style.css";
 
-import Login from "./Components/pages/Sign/Login";
-import SignUp from "./Components/pages/Sign/SignUp";
-import Home from "./Components/pages/Video/Home";
+// import Login from "./Components/pages/Sign/Login";
+// import SignUp from "./Components/pages/Sign/SignUp";
+// import Home from "./Components/pages/Video/Home";
 import Layout from "./Layout";
 function App() {
   return (
     <>
       <Layout>
-        <Home />
+        {/* <Home />
         <SignUp />
-        <Login />
+        <Login /> */}
       </Layout>
     </>
   );
