7b0d90dead43aa7d79c6accd7e26e8ef49389a78
Shariful Islam <sajukhan12905@gmail.com>
2024-07-29 19:52:43 +0600

    hello

---
+++ src/fireBaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MASSAGINGSENDERID,
+  appId: process.env.REACT_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
