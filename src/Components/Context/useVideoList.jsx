c6101f049376f4bd3a9301abd4fdcdf78862e9c9
Shariful Islam <sajukhan12905@gmail.com>
2024-08-19 20:23:40 +0600

    Add Infinite Scroll

--- src/Components/Context/useVideoList.jsx
+++ src/Components/Context/useVideoList.jsx
@@ -1,24 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
-const useVideoList = () => {
+import {
+  get,
+  getDatabase,
+  limitToFirst,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+} from "firebase/database";
+const useVideoList = (page) => {
   const [loding, setLoding] = useState(false);
   const [error, setError] = useState(null);
   const [data, setData] = useState();
+  const [hasmore, setHasmore] = useState(false);
   useEffect(() => {
     async function renderVideos() {
       setLoding(true);
       const db = getDatabase();
       const videosRef = ref(db, "videos");
-      const videoQuery = query(videosRef, orderByKey());
+      const videoQuery = query(
+        videosRef,
+        orderByKey(),
+        startAt("0"),
+        limitToFirst(8 + page)
+      );
       try {
         const snapShot = await get(videoQuery);
         setLoding(false);
         if (snapShot.exists()) {
           setData(Object.values(snapShot.val()));
+          setHasmore(true);
         } else {
-          throw new Error("Not Found!");
+          setHasmore(false);
         }
       } catch (error) {
         setLoding(false);
@@ -27,8 +42,8 @@ const useVideoList = () => {
       }
     }
     renderVideos();
-  }, []);
-  return { loding, error, data };
+  }, [page]);
+  return { loding, error, data, hasmore };
 };
 
 export default useVideoList;
--- src/Components/pages/Video/Videos.jsx
+++ src/Components/pages/Video/Videos.jsx
@@ -2,14 +2,30 @@
 import style from "./style.module.css";
 import Video from "./Video";
 import useVideoList from "../../Context/useVideoList";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Videos = () => {
-  const { loding, error, data } = useVideoList();
+  const [page, setPage] = useState(0);
+  const { loding, error, data, hasmore } = useVideoList(page);
+ 
   return (
-    <div className={style.videos}>
-      {data?.map((video) => {
-        return <Video item={video} key={video.youtubeID} />;
-      })}
+    <div>
+      {data && (
+        <InfiniteScroll
+          dataLength={data?.length}
+          hasMore={hasmore}
+          next={() => setPage(page+8)}
+          className={style.videos}
+        >
+          {data?.map((video) => {
+            return <Video item={video} key={video.youtubeID} />;
+          })}
+        </InfiniteScroll>
+      )}
+      {!loding && data?.length === 0 && <div>No Data Found</div>}
+      {error && <div>There is a Error</div>}
+      {loding && <div> Loding...</div>}
     </div>
   );
 };
