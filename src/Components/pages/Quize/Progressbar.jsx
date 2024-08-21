e298cf9bc10777f6779480c4f3a1668146e296cd
Shariful Islam <sajukhan12905@gmail.com>
2024-08-22 01:20:23 +0600

    Adding next prev and progress

--- src/Components/pages/Quize/Progressbar.jsx
+++ src/Components/pages/Quize/Progressbar.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import styles from "./style.module.css";
 import { Link } from "react-router-dom";
 
-const Progressbar = () => {
+const Progressbar = ({ next, prev, progress }) => {
   return (
     <>
       <div className={styles.progressBar}>
-        <div className={styles.backButton}>
+        <div onClick={prev} className={styles.backButton}>
           <span className="material-icons-outlined"> arrow_back </span>
         </div>
         <div className={styles.rangeArea}>
-          <div className={styles.tooltip}>24% Complete!</div>
+          <div className={styles.tooltip}>{progress} Complete!</div>
           <div className={styles.rangeBody}>
-            <div className={styles.progress} style={{ width: "20%" }}></div>
+            <div
+              className={styles.progress}
+              style={{ width: `${progress}` }}
+            ></div>
           </div>
         </div>
-        <Link to={"/result"}>
-          <button className={`${styles.button} ${styles.next}`}>
-            <span>Next Question</span>
-            <span className="material-icons-outlined"> arrow_forward </span>
-          </button>
-        </Link>
+        <button onClick={next} className={`${styles.button} ${styles.next}`}>
+          <span>Next Question</span>
+          <span className="material-icons-outlined"> arrow_forward </span>
+        </button>
       </div>{" "}
     </>
   );
--- src/Components/pages/Quize/Quize.jsx
+++ src/Components/pages/Quize/Quize.jsx
@@ -9,7 +9,7 @@ import reducer from "./useReducers";
 const Quize = () => {
   const location = useLocation();
   const id = location.state.youtubeID;
-  const [currentQuestion, setCurrentQueston] = useState(2);
+  const [currentQuestion, setCurrentQueston] = useState(0);
   const { loding, error, quize } = useQuizList(id);
   const [qna, dispatch] = useReducer(reducer, quize);
   const handleChange = (e, i) => {
@@ -23,11 +23,32 @@ const Quize = () => {
     });
   };
 
+  function nextQ() {
+    if (currentQuestion + 1 < quize.length) {
+      setCurrentQueston((q) => q + 1);
+    }
+  }
+
+  const percentage =
+    quize && quize.length > 0
+      ? ((currentQuestion + 1) / quize.length) * 100
+      : 0;
+
+  function prevQ() {
+    if (currentQuestion >= 1 && currentQuestion <= quize.length) {
+      setCurrentQueston((q) => q - 1);
+    }
+  }
+
   useEffect(() => {
     dispatch({ type: "questions", payload: quize });
   }, [quize]);
   return (
     <div>
+      {loding && <div>Loding...</div>}
+      {error && (
+        <div> There was an Error In your Life, Not In my Program. </div>
+      )}
       <h1>{qna && qna[currentQuestion].title}</h1>
 
       <h4>Question can have multiple answers</h4>
@@ -35,7 +56,7 @@ const Quize = () => {
         options={qna && qna[currentQuestion].options}
         handleChange={handleChange}
       />
-      <Progressbar />
+      <Progressbar next={nextQ} progress={percentage} prev={prevQ} />
       <MiniPlayer />
     </div>
   );
